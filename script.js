1b0f5b337b396748d054a74b2e2b8ea0401f01b4
timaelkhammas <99289981+timaelkhammas@users.noreply.github.com>
2024-05-26 23:05:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var video = document.getElementById('videoElement');
+
+  if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Something went wrong!");
+      });
+  }
+});
